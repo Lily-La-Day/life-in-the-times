99cfdcbae33eb01a1aaa3d6d6a5d7debe69f5d99
mohamedlafiteh <m_lafiteh@yahoo.com>
2021-02-21 15:10:26 +0000

    Create two js files and img cards folder

---
+++ js/post_cards.js
@@ -0,0 +1,7 @@
+//const array = require('./post_cards_data.js');
+
+import {post_cards_array} from './post_cards_data.js';
+
+console.log(post_cards_array);
+
+//Uncaught SyntaxError: Cannot use import statement outside a module
