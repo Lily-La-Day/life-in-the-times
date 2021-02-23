048cf5cc3ac120407133564626a4e5ed862296cc
mohamedlafiteh <m_lafiteh@yahoo.com>
2021-02-23 14:58:14 +0000

    Add functionality to load more button, amages  and style

--- js/post_cards.js
+++ js/post_cards.js
@@ -1,7 +1,30 @@
-//const array = require('./post_cards_data.js');
 
-import {post_cards_array} from './post_cards_data.js';
+import {postCardsArray as cards} from './postCardsData.js';
 
-console.log(post_cards_array);
 
-//Uncaught SyntaxError: Cannot use import statement outside a module
+let content= document.querySelector(".masonry-container");
+let loadMoreButton = document.querySelector(".load-more-cards-btn");
+
+let currentIndex=0;
+ loadMore()
+loadMoreButton.addEventListener("click", loadMore);
+
+function loadMore(){
+        
+    let maxResult= 10;
+
+    for (let i = 0; i < maxResult; i++) {
+        $(content).append(    
+            "<div class='col-sm-6 col-md-4 col-lg-4 mb-60 mb-xs-40 '>"+
+                "<div class='post-prev-img'>"+
+                "<a>"+"<img src='"+ cards[i+currentIndex].img+"'  alt='' />"+"</a>" 
+                +"</div></div>"                  
+        )   
+    }
+    currentIndex+=maxResult;
+
+     if(currentIndex >=cards.length){
+            $(loadMoreButton).hide();
+        }
+}
+
