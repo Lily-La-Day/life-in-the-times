09244884c028f129f1fc94aeb56fc874b32f09d8
mohamedlafiteh <m_lafiteh@yahoo.com>
2021-02-24 00:14:26 +0000

    Add showimg functionality and style to show image when click

--- js/post_cards.js
+++ js/post_cards.js
@@ -6,20 +6,25 @@ let content= document.querySelector(".masonry-container");
 //Grab the load more postcards button.
 let loadMoreButton = document.querySelector(".load-more-cards-btn");
 
+//  insert the image inside the modal.
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
 let currentIndex=0;
 //Call the load function to load the first ten cards.
  loadMore()
 loadMoreButton.addEventListener("click", loadMore);
 // Load function for loading the 30 cards
 function loadMore(){
-        
     let maxResult= 10;
 // This loop append ten cards each time
     for (let i = 0; i < maxResult; i++) {
         $(content).append(    
             "<div class='col-sm-6 col-md-4 col-lg-4 mb-60 mb-xs-40 '>"+
                 "<div class='post-prev-img'>"+
-                "<a>"+"<img src='"+ cards[i+currentIndex].img+"'  alt='' />"+"</a>" 
+                "<div ><img class='p-cards' src='"+ cards[i+currentIndex].img+"'  alt='' /></div>"
                 +"</div></div>"                  
         )   
     }
@@ -28,5 +33,22 @@ function loadMore(){
      if(currentIndex >=cards.length){
             $(loadMoreButton).hide();
         }
+        //Get each ten cards after the first load and clicking add event listener to them
+     var img = document.querySelectorAll(".p-cards");
+     for(let i=0;i<img.length;i++){
+     img[i].addEventListener("click", showImg);
+
+}
+}
+
+//This function will view the image
+ function showImg(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+// close the modal function
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
