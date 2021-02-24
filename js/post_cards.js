4db7b9051b88cc2fd630162ea0d823bca51820bf
Lily-La-Day <lily.keeling@gmail.com>
2021-02-24 11:32:47 +0000

    adding styles for modal and changing HTML layout slightly

--- js/post_cards.js
+++ js/post_cards.js
@@ -3,8 +3,13 @@ import { postCardsArray as cards } from "./postCardsData.js";
 let content = document.querySelector(".postcards");
 let loadMoreButton = document.querySelector(".load-more-cards-btn");
 
-const positions = ['right-side', 'left-side', 'up-side', 'down-side']
-const sizes = ['small-size', 'medium-size', 'large-size']
+const positions = ["right-side", "left-side", "up-side", "down-side", "center-x", 'center-y'];
+const sizes = ["small-size", "medium-size", "large-size"];
+//  insert the image inside the modal.
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 let currentIndex = 0;
 loadMore();
 loadMoreButton.addEventListener("click", loadMore);
@@ -13,23 +18,42 @@ function loadMore() {
   let maxResult = 10;
 
   for (let i = 0; i < maxResult; i++) {
-    // Here I am generating a number between 0-2/3 to get a random element from the 
+    // Here I am generating a number between 0-2/3 to get a random element from the
     // style arrays above and then applying one size and one position to each added postcard
-    const randomClass = positions[Math.floor(Math.random() * (3 + 1))]  
-    const randomSize = sizes[Math.floor(Math.random() * (2 + 1))]  
+    const randomPosition = positions[Math.floor(Math.random() * (5 + 1))];
+    const randomSize = sizes[Math.floor(Math.random() * (2 + 1))];
     $(content).append(
-      "<div class='post-prev-img " + randomClass + " " + randomSize + "'>" +
-        "<a>" +
-        "<img src='" +
+      "<div class='post-prev-img " +
+        randomPosition +
+        " " +
+        randomSize +
+        "'>" +
+        "<div ><img class='p-cards' src='" +
         cards[i + currentIndex].img +
-        "'  alt='' />" +
-        "</a>" +
+        "'  alt='' /></div>" +
         "</div>"
     );
   }
   currentIndex += maxResult;
-
+  // Check if the 30 cards rendered and it will hide the button.
   if (currentIndex >= cards.length) {
     $(loadMoreButton).hide();
   }
+  //Get each ten cards after the first load and clicking add event listener to them
+  var img = document.querySelectorAll(".p-cards");
+  for (let i = 0; i < img.length; i++) {
+    img[i].addEventListener("click", showImg);
+  }
 }
+
+//This function will view the image
+function showImg() {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+// close the modal function
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
