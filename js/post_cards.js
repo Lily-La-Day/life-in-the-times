a0f7536a14405665e5507b4f7e2da3a9da9d8d3b
Lily-La-Day <lily.keeling@gmail.com>
2021-02-24 10:38:21 +0000

    WIP postcard experiment using CSS grid and random classes

--- js/post_cards.js
+++ js/post_cards.js
@@ -1,30 +1,35 @@
+import { postCardsArray as cards } from "./postCardsData.js";
 
-import {postCardsArray as cards} from './postCardsData.js';
-
-
-let content= document.querySelector(".masonry-container");
+let content = document.querySelector(".postcards");
 let loadMoreButton = document.querySelector(".load-more-cards-btn");
 
-let currentIndex=0;
- loadMore()
+const positions = ['right-side', 'left-side', 'up-side', 'down-side']
+const sizes = ['small-size', 'medium-size', 'large-size']
+let currentIndex = 0;
+loadMore();
 loadMoreButton.addEventListener("click", loadMore);
 
-function loadMore(){
-        
-    let maxResult= 10;
+function loadMore() {
+  let maxResult = 10;
 
-    for (let i = 0; i < maxResult; i++) {
-        $(content).append(    
-            "<div class='col-sm-6 col-md-4 col-lg-4 mb-60 mb-xs-40 '>"+
-                "<div class='post-prev-img'>"+
-                "<a>"+"<img src='"+ cards[i+currentIndex].img+"'  alt='' />"+"</a>" 
-                +"</div></div>"                  
-        )   
-    }
-    currentIndex+=maxResult;
+  for (let i = 0; i < maxResult; i++) {
+    // Here I am generating a number between 0-2/3 to get a random element from the 
+    // style arrays above and then applying one size and one position to each added postcard
+    const randomClass = positions[Math.floor(Math.random() * (3 + 1))]  
+    const randomSize = sizes[Math.floor(Math.random() * (2 + 1))]  
+    $(content).append(
+      "<div class='post-prev-img " + randomClass + " " + randomSize + "'>" +
+        "<a>" +
+        "<img src='" +
+        cards[i + currentIndex].img +
+        "'  alt='' />" +
+        "</a>" +
+        "</div>"
+    );
+  }
+  currentIndex += maxResult;
 
-     if(currentIndex >=cards.length){
-            $(loadMoreButton).hide();
-        }
+  if (currentIndex >= cards.length) {
+    $(loadMoreButton).hide();
+  }
 }
-
