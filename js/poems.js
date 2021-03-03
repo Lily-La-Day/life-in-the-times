50b355ea10b49232711232d2125c6e9c29a02ecc
Lily-La-Day <lily.keeling@gmail.com>
2021-03-03 15:29:08 +0000

    fixing misnamed var

--- js/poems.js
+++ js/poems.js
@@ -5,6 +5,7 @@
 // I've added the audio files to an object here rather than in the HTML, we will need to swap these for the real
 // translatings and add a seperate one for each poem :) 
 const translations = {
+  "poem-1-body": {
   english:
     "http://www.culturedub.com/assets/04-Forward.mp3",
   arabic: "http://www.culturedub.com/assets/04-Forward.mp3",
@@ -12,6 +13,7 @@ const translations = {
     "http://www.culturedub.com/assets/04-Moringa-JahYu-Remix-feat-BaNdula-1.mp3",
   kurdish:
     "https://drive.google.com/drive/folders/1vOZ1GoAcPjeiRGh4VivuM-fxnVASy37a",
+  }
 };
 
 const initPlayer = () => {
@@ -20,6 +22,9 @@ var audio = document.getElementById("player");
 
     var source = document.getElementById("mp3_src");
 
+    // creating a poem variable
+
+
     // This is just a Bootstrap tooltip I've added to show "translations" on hover so that the user knows to click 
     $('[data-toggle="tooltip"]').tooltip();
 
@@ -27,17 +32,18 @@ var audio = document.getElementById("player");
 
     let playing = false
     //Setting the default to English 
-    source.src = translations['english'];
-    audio.load();
+    let language = 'english'
 
+
+    audio.load();
 
     // The below needs a bit of refactoring to use variable names rather than DOM selectors and we also need to
     // add logic so that this works for different poems and not just one. 
 
-
     //On Change funcition to switch langauge
-    function change(language) {
-      source.src = translations[language];
+    function change(lang) {
+      language = lang
+
     }
 
     $("#selection").on("change", function () {
@@ -53,7 +59,14 @@ var audio = document.getElementById("player");
 
     //toggling play/pause and the icon to match
 
-    const togglePlay = () => {
+    const togglePlay = (e) => {
+      // here we access the first class name of the play button we click, which will correspond to the poem key in 
+      // the translation object
+      poem = e.currentTarget.classList[0]
+      console.log(poem)
+      console.log(language)
+      source.src = translations[poem][language];
+
       if (playing == false) {
         document.querySelector('.play').classList.remove('fa-play')
         document.querySelector('.play').classList.add('fa-pause')
