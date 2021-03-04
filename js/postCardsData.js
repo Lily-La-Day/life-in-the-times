b8b4b6640e8698a13b4fff0ca97caf14eb8fdb2c
mohamedlafiteh <m_lafiteh@yahoo.com>
2021-03-04 10:10:51 +0000

    Add the cards

--- js/postCardsData.js
+++ js/postCardsData.js
@@ -1,92 +1,92 @@
 export let postCardsArray = [
     {id:1,
-    img:".././images/cards/card1.png"
+    img:".././images/cards/card1.jpg"
     },
      {id:2,
-    img:".././images/cards/card2.png"
+    img:".././images/cards/card2.jpg"
     },
      {id:3,
-    img:".././images/cards/card3.png"
+    img:".././images/cards/card3.jpg"
     },
      {id:4,
-    img:".././images/cards/card4.png"
+    img:".././images/cards/card4.jpg"
     },
      {id:5,
-    img:".././images/cards/card5.png"
+    img:".././images/cards/card5.jpg"
     },
      {id:6,
-    img:".././images/cards/card6.png"
+    img:".././images/cards/card6.jpg"
     },
      {id:7,
-    img:".././images/cards/card7.png"
+    img:".././images/cards/card7.jpg"
     },
      {id:8,
-    img:".././images/cards/card8.png"
+    img:".././images/cards/card8.jpg"
     },
      {id:9,
-    img:".././images/cards/card9.png"
+    img:".././images/cards/card9.jpg"
     },
      {id:10,
-    img:".././images/cards/card10.png"
+    img:".././images/cards/card10.jpg"
     },
         {id:11,
-    img:".././images/cards/card11.png"
+    img:".././images/cards/card11.jpg"
     },
      {id:12,
-    img:".././images/cards/card12.png"
+    img:".././images/cards/card12.jpg"
     },
      {id:13,
-    img:".././images/cards/card13.png"
+    img:".././images/cards/card13.jpg"
     },
      {id:14,
-    img:".././images/cards/card14.png"
+    img:".././images/cards/card14.jpg"
     },
      {id:15,
-    img:".././images/cards/card15.png"
+    img:".././images/cards/card15.jpg"
     },
      {id:16,
-    img:".././images/cards/card16.png"
+    img:".././images/cards/card16.jpg"
     },
      {id:17,
-    img:".././images/cards/card17.png"
+    img:".././images/cards/card17.jpg"
     },
      {id:18,
-    img:".././images/cards/card18.png"
+    img:".././images/cards/card18.jpg"
     },
      {id:19,
-    img:".././images/cards/card19.png"
+    img:".././images/cards/card19.jpg"
     },
      {id:20,
-    img:".././images/cards/card20.png"
+    img:".././images/cards/card20.jpg"
     },
      {id:21,
-    img:".././images/cards/card21.png"
+    img:".././images/cards/card21.jpg"
     },
         {id:22,
-    img:".././images/cards/card22.png"
+    img:".././images/cards/card22.jpg"
     },
      {id:23,
-    img:".././images/cards/card23.png"
+    img:".././images/cards/card23.jpg"
     },
      {id:24,
-    img:".././images/cards/card24.png"
+    img:".././images/cards/card24.jpg"
     },
      {id:25,
-    img:".././images/cards/card25.png"
+    img:".././images/cards/card25.jpg"
     },
      {id:26,
-    img:".././images/cards/card26.png"
+    img:".././images/cards/card26.jpg"
     },
      {id:27,
-    img:".././images/cards/card27.png"
+    img:".././images/cards/card27.jpg"
     },
      {id:28,
-    img:".././images/cards/card28.png"
+    img:".././images/cards/card28.jpg"
     },
      {id:29,
-    img:".././images/cards/card29.png"
+    img:".././images/cards/card29.jpg"
     },
      {id:30,
-    img:".././images/cards/card30.png"
+    img:".././images/cards/card30.jpg"
     }
 ]
