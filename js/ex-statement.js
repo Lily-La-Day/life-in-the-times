2aa600694d430c3096a89b3526eab81f89af2219
mohamedlafiteh <m_lafiteh@yahoo.com>
2021-03-06 16:07:56 +0000

    Add languages to html and style them

--- js/ex-statement.js
+++ js/ex-statement.js
@@ -1,59 +1,31 @@
-
-
-let ul = document.getElementById("list-container");
-let items = ul.getElementsByTagName("li");
+let items = document.querySelectorAll(".l-list1");
 let langTag = document.getElementById("ex-lang");
 
+defaultLanguage();
 
-ul.addEventListener("click", ulListClick);
-
-let english = "JavaScript is a scripting  orprogramming language that allows\
- you to implement complex features on web pages  every time a web\
-  page does more than just sit there and display static information for you."
-
-let arabic = "المادة 2 لكلو أرأي  أو أي رأي آخر، أوالوطني أو الإجتماعي أوأو الميلاد \
-   السياسي   حق التمتع بكافة الحقوق  لوطني أو الإجتماعيوالحريات الواردة\
- في هذا الإعلان، دون أي تمييز،و اللو أرأي  أو أي رأي آخر، أوالوطني أو الإجتماعي أوأو الميلاد "
-
-let kurdish = "Her şahsın öğrenim hakkı vardır. Öğrenim hiç olmazsa ilk ve temel safhalarında \
-parasızdır. İlk öğretim mecburidir. Teknik ve mesleki öğretimden herkes istifade edebilmelidir. Yüksek ö\
- Öğretim insan şahs saygının kuvvetlenmesini hedef almalıdır.";
-
-
-let urdu = "Her şahsın öğrenim hakkı vardır. Öğrenim hiç olmazsa ilk ve temel safhalarında \
-parasızdır. İlk öğretim mecburidir. Teknik ve mesleki öğretimden herkes istifade edebilmelidir. Yüksek ö\
- Öğretim insan şahs saygının kuvvetlenmesini hedef almalıdır.";
-
-
-
-  for (var i = 0; i < items.length; ++i) {
+for (var i = 0; i < items.length; i++) {
   // do something with items[i], which is a <li> element
-  items[i].addEventListener("click", getEventTarget);
-  
+  items[i].addEventListener("click", onLanguageClick);
 }
 
-function getEventTarget(e) {
-    e = e || window.event;
-    return e.target || e.srcElement; 
+function defaultLanguage() {
+  langTag.style.display="block";
+  let tr1 = document.querySelectorAll(".translations-lang")
+    for(let i = 0; i< tr1.length-1; i++ ) {
+        tr1[i+1].style.display="none"
+    }
 }
 
-function ulListClick(event) {
-    let target = getEventTarget(event);
-    let lang = target.innerHTML;
-    if(lang === "English") {
-        langTag.innerText=english
-    } else if(lang === "Arabic") {
-        langTag.innerText=arabic
-    }else if(lang === "Urdu") {
-               langTag.innerText=urdu
+function onLanguageClick(event) {
+    let lang = event.target.innerHTML;
+    let tr2 = document.querySelectorAll(".translations-lang")
+    let pArray=[...tr2]
+    let lArr = ["English","Arabic","Urdu", "Kurdish"];
 
-    } else if(lang === "Kurdish") {
-          langTag.innerText=kurdish
-    } else{
-         langTag.innerText=english
+    for(let i = 0; i < tr2.length; i++ ) {
+        tr2[i].style.display="none";
+        if(lArr.indexOf(lang)==i) {
+            pArray[i].style.display="block"
+        }
     }
 };
-
-
-
-
