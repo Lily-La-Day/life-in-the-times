b8cc3705e957456ce4d4fd6b68618a9fa97b42ee
Andrei <andsemenov@gmail.com>
2021-03-09 20:41:09 +0000

    changed placeholder for last video, added titles for video

--- js/ten-x-videos-data.js
+++ js/ten-x-videos-data.js
@@ -5,8 +5,20 @@ export const videoData = [
   { videoId: "dtR2U-KsE3g", titleOfVideo: "Comfort & Fakhirya" },
   { videoId: "Wqx06DtYIZs", titleOfVideo: "Jabin" },
   { videoId: "9uIk_91GQYI", titleOfVideo: "Rosemary" },
-  { videoId: "HSsqzzuGTPo", titleOfVideo: "No title 1" },
-  { videoId: "6obKeeH7cQc", titleOfVideo: "No title 2" },
-  { videoId: "TRgqtaYb4sU", titleOfVideo: "No title 3" },
-  { videoId: "9Ej-0VRWmI8", titleOfVideo: "No title 4" },
+  {
+    videoId: "HSsqzzuGTPo",
+    titleOfVideo: "Introduction and 1. Between Two Worlds",
+  },
+  {
+    videoId: "6obKeeH7cQc",
+    titleOfVideo: "2. 100 Words From My Heart & 3. Campfire ",
+  },
+  {
+    videoId: "TRgqtaYb4sU",
+    titleOfVideo: "4. Country of My Dreams & 5. A Journey to Freedom",
+  },
+  {
+    videoId: "9Ej-0VRWmI8",
+    titleOfVideo: "6. Comes the Morning Brightness & 7. The Bubble",
+  },
 ];
