56e165dc2f0c3db05d47f371eb785aad91bc5c9e
Andrei <andsemenov@gmail.com>
2021-03-11 12:54:20 +0000

    changed titles of videos

--- js/ten-x-videos-data.js
+++ js/ten-x-videos-data.js
@@ -7,18 +7,18 @@ export const videoData = [
   { videoId: "9uIk_91GQYI", titleOfVideo: "Rosemary" },
   {
     videoId: "HSsqzzuGTPo",
-    titleOfVideo: "Introduction and 1. Between Two Worlds",
+    titleOfVideo: "Introduction & Between Two Worlds",
   },
   {
     videoId: "6obKeeH7cQc",
-    titleOfVideo: "2. 100 Words From My Heart & 3. Campfire ",
+    titleOfVideo: "100 Words From My Heart & Campfire ",
   },
   {
     videoId: "TRgqtaYb4sU",
-    titleOfVideo: "4. Country of My Dreams & 5. A Journey to Freedom",
+    titleOfVideo: "Country of My Dreams & A Journey to Freedom",
   },
   {
     videoId: "9Ej-0VRWmI8",
-    titleOfVideo: "6. Comes the Morning Brightness & 7. The Bubble",
+    titleOfVideo: "Comes the Morning Brightness & The Bubble",
   },
 ];
