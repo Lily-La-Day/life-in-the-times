97e972573071a4e1213b7810502ff35b57a7cfe6
Marina <mara.berezin@gmail.com>
2021-03-12 19:27:02 +0000

    draft- share.js, list of links

---
+++ js/share-link.js
@@ -0,0 +1,9 @@
+function copyLink() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+}
+
+document.querySelector("#myInput").addEventListener("click", copy);
