afe5a25ce078920dce1d1d69026a737ebc070131
Andrei <andsemenov@gmail.com>
2021-03-20 09:03:10 +0000

    added line through

--- js/header-sketches.js
+++ js/header-sketches.js
@@ -1,34 +1,35 @@
 // document.body.click();
 
-const enterButtons = document.querySelectorAll('.enter')
-const enterContainer = document.querySelector('.enter-container')
-const whole = document.querySelector('.whole')
+const enterButtons = document.querySelectorAll(".enter");
+const enterContainer = document.querySelector(".enter-container");
+const whole = document.querySelector(".whole");
 
-enterButtons.forEach(language => language.addEventListener("click", playAudio))
+enterButtons.forEach((language) =>
+  language.addEventListener("click", playAudio)
+);
 
-const audio = new Audio("./audio/header/Life in the Time Header_1-2.mp3")
-
+const audio = new Audio("./audio/header/Life in the Time Header_1-2.mp3");
 
 function playAudio() {
-  whole.classList.remove('hide')
-  enterContainer.classList.add('hide')
+  whole.classList.remove("hide");
+  enterContainer.classList.add("hide");
   audio.play();
 }
 
-
 function toggleAudio() {
-  if (audio.paused){
-  audio.play();
+  if (audio.paused) {
+    document.querySelector(".audio-icon").src = "../images/audio-icon-hero.png";
+    audio.play();
   } else {
+    document.querySelector(".audio-icon").src =
+      "../images/audio-icon-hero-crossed.png";
     audio.pause();
   }
 }
 
-
 const imageSketch = document.querySelector("#home");
 imageSketch.style.backgroundImage = `url('images/header-sketches/Header-Sketch-01.jpg')`;
-document.querySelector('.audio-icon').addEventListener('click', toggleAudio)
-document.querySelector('.audio-icon').addEventListener('click', toggleAudio)
+document.querySelector(".audio-icon").addEventListener("click", toggleAudio);
 
 let counter = 1;
 function loopSketches() {
@@ -37,9 +38,9 @@ function loopSketches() {
       counter++;
       //console.log(counter);
       imageSketch.style.backgroundImage = `url('images/header-sketches/Header-Sketch-0${counter}.jpg')`;
-      imageSketch.classList.add('back-sketch')
-      imageSketch.style.height = '80vh';
-      imageSketch.style.backgroundPosition = 'fixed';
+      imageSketch.classList.add("back-sketch");
+      imageSketch.style.height = "80vh";
+      imageSketch.style.backgroundPosition = "fixed";
 
       loopSketches();
     }, 5000);
